9072951cd972f872708d585947516b8198bc12dc
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-04-05 18:06:51 +0200

    New function return-file

--- src/emender_service/server.clj
+++ src/emender_service/server.clj
@@ -2,9 +2,24 @@
 
 (require '[ring.util.response     :as http-response])
 
+(defn return-file
+    "Creates HTTP response containing content of specified file.
+     Special value nil / HTTP response 404 is returned in case of any I/O error."
+    [file-name content-type]
+    (let [file (new java.io.File "www" file-name)]
+        (println-and-flush "Returning file " (.getAbsolutePath file))
+        (if (.exists file)
+            (-> (http-response/response file)
+                (http-response/content-type content-type))
+            (println "return-file(): can not access file: " (.getName file)))))
+
 (defn handler
     "Handler that is called by Ring for all requests received from user(s)."
     [request]
     (println "request URI: " (request :uri))
-    )
+    (let [uri (request :uri)]
+        (condp = uri
+            "/favicon.ico"       (return-file "favicon.ico" "image/x-icon")
+            "/"                  (render-front-page request)
+    )))
 
