0ea91c86ed78e993b9e786b139f4873852aecf0b
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-04-05 17:49:50 +0200

    Core module, CLI options handling, server initialization etc.

--- src/emender_service/core.clj
+++ src/emender_service/core.clj
@@ -1,7 +1,58 @@
 (ns emender-service.core
-  (:gen-class))
+    "Core module that contains -main function called by Leiningen to start the application.")
+
+(require '[ring.adapter.jetty      :as jetty])
+(require '[ring.middleware.params  :as http-params])
+(require '[ring.middleware.cookies :as cookies])
+
+(require '[clojure.tools.cli       :as cli])
+
+(require '[emender-service.server  :as server])
+
+(def default-port
+    "3000")
+
+(def cli-options
+    "Definitions of all command line options currenty supported."
+    ;; an option with a required argument
+    [["-p" "--port   PORT"    "port number"   :id :port]])
+
+(def app
+    "Definition of a Ring-based application behaviour."
+    (-> server/handler            ; handle all events
+        cookies/wrap-cookies      ; we need to work with cookies
+        http-params/wrap-params)) ; and to process request parameters, of course
+
+(defn start-server
+    "Start the HTTP server on the specified port."
+    [port]
+    (println "Starting the server at the port: " port)
+    (jetty/run-jetty app {:port (read-string port)}))
+
+(defn get-and-check-port
+    "Accepts port number represented by string and throws AssertionError
+     if port number is outside defined range."
+    [port]
+    (let [port-number (. Integer parseInt port)]
+        (assert (> port-number 0))
+        (assert (< port-number 65536))
+        port))
+
+(defn get-port
+    "Returns specified port or default port if none is specified on the command line."
+    [specified-port]
+    (if (or (not specified-port) (not (string? specified-port)) (empty? specified-port))
+        default-port
+        (get-and-check-port specified-port)))
+
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+    "Entry point to the Emender service server"
+    [& args]
+    (let [all-options   (cli/parse-opts args cli-options)
+          options       (all-options :options)
+          port          (options :port)]
+          (start-server (get-port port))))
+
+; finito
+
