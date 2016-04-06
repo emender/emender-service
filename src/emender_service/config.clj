60066a0a98eedd26043dc9cc15eb340dc7a48177
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-04-06 13:03:02 +0200

    Config module

---
+++ src/emender_service/config.clj
@@ -0,0 +1,39 @@
+;
+;  (C) Copyright 2016  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
+(ns emeder-service.config)
+
+(require '[clojure.pprint :as pprint])
+
+(require '[clojure-ini.core :as clojure-ini])
+
+(defn parse-int
+    "Parse the given string as an integer number."
+    [string]
+    (java.lang.Integer/parseInt string))
+
+(defn parse-float
+    "Parse the given string as a float number."
+    [string]
+    (java.lang.Float/parseFloat string))
+
+(defn load-configuration
+    "Load configuration from the provided INI file."
+    [ini-file-name]
+    (-> (clojure-ini/read-ini ini-file-name :keywordize? true)
+    ))
+
+(defn print-configuration
+    "Print actual configuration to the output."
+    [configuration]
+    (pprint/pprint configuration))
+
