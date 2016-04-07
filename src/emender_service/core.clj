ff21df853b57bd57026b864f8a3b80f76c931c13
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-04-07 11:55:18 +0200

    Added inject-configuration middleware into ring app

--- src/emender_service/core.clj
+++ src/emender_service/core.clj
@@ -1,3 +1,15 @@
+;
+;  (C) Copyright 2016  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
 (ns emender-service.core
     "Core module that contains -main function called by Leiningen to start the application.")
 
@@ -7,7 +19,9 @@
 
 (require '[clojure.tools.cli       :as cli])
 
-(require '[emender-service.server  :as server])
+(require '[emender-service.server      :as server])
+(require '[emender-service.config      :as config])
+(require '[emender-service.middleware  :as middleware])
 
 (def default-port
     "3000")
@@ -17,9 +31,13 @@
     ;; an option with a required argument
     [["-p" "--port   PORT"    "port number"   :id :port]])
 
+; we need to load the configuration in advance so the 'app' could use it
+(def configuration (config/load-configuration "emender-service.ini"))
+
 (def app
     "Definition of a Ring-based application behaviour."
     (-> server/handler            ; handle all events
+        (middleware/inject-configuration configuration) ; inject configuration structure into the parameter
         cookies/wrap-cookies      ; we need to work with cookies
         http-params/wrap-params)) ; and to process request parameters, of course
 
