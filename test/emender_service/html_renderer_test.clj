dff18a0b718f8e7b73fc93393db1ff576713def0
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-04-07 11:57:44 +0200

    Html-renderer test module

---
+++ test/emender_service/html_renderer_test.clj
@@ -0,0 +1,7 @@
+(ns emender-service.html-renderer-test
+  (:require [clojure.test :refer :all]
+            [emender-service.html-renderer :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
