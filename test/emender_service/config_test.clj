99010c10ef91000e43dac262cb1a329686f46ba7
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-04-07 13:54:51 +0200

    Stub for tests for the config module

---
+++ test/emender_service/config_test.clj
@@ -0,0 +1,7 @@
+(ns emender-service.config-test
+  (:require [clojure.test :refer :all]
+            [emender-service.config :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
