45709ad8cda226f0df06ae3417661a49edcc81cd
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-04-07 11:58:21 +0200

    irc-bot test module

---
+++ test/emender_service/irc_bot_test.clj
@@ -0,0 +1,7 @@
+(ns emender-service.irc-bot-test
+  (:require [clojure.test :refer :all]
+            [emender-service.irc-bot :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
