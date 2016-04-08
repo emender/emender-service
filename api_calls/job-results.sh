afcb3946f9d41c524b7a2222df03a8ad177688c4
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-04-08 17:23:59 +0200

    Script to send test job results to the service

---
+++ api_calls/job-results.sh
@@ -0,0 +1,3 @@
+#!/bin/env bash
+curl -X POST --header "Content-Type: application/json" -d "@results.json" -v "localhost:3000/v1/job-results/doc-Red_Hat_Developer_Toolset-1-Release_Notes-en-US+(test)"
+
