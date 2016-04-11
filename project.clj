678dd05b89452bc44e8051f51b50476073599314
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-04-11 14:28:15 +0200

    New dependency: Clojure-SQL and SQL lite driver

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
     :license {:name "Eclipse Public License"
               :url "http://www.eclipse.org/legal/epl-v10.html"}
     :dependencies [[org.clojure/clojure "1.7.0"]
+                   [org.clojure/java.jdbc "0.3.5"]
                    [org.clojure/tools.cli "0.3.1"]
+                   [org.xerial/sqlite-jdbc "3.7.2"]
                    [clojure-ini "0.0.1"]
                    [ring/ring-core "1.3.2"]
                    [ring/ring-jetty-adapter "1.3.2"]
