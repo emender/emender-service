323237a86bf5c8c835bab06f1b4575aaaedf383e
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-04-11 15:58:37 +0200

    Functions to records requests, job events and job results into database

--- src/emender_service/db_interface.clj
+++ src/emender_service/db_interface.clj
@@ -13,3 +13,44 @@
 (ns emender-service.db-interface
     "Namespace that contains configuration of all JDBC sources.")
 
+(require '[clojure.java.jdbc       :as jdbc])
+
+(require '[emender-service.db-spec :as db-spec])
+
+
+
+(def date-format
+    (new java.text.SimpleDateFormat "yyyy-MM-dd HH:mm:ss"))
+
+(defn format-date
+    [date]
+    (.format date-format date))
+
+(defn format-current-date
+    []
+    (format-date (new java.util.Date)))
+
+(defn record-request-log
+    [uri ipaddress datetime params useragent]
+    (try
+        (jdbc/insert! db-spec/emender-service-db
+                      :requests {:uri uri :ipaddress ipaddress :datetime datetime :params params :useragent useragent})
+        (catch Exception e
+            (println e))))
+
+(defn record-job-event
+    [job-name datetime operation]
+    (try
+        (jdbc/insert! db-spec/emender-service-db
+                      :jobs {:job job-name :datetime datetime :operation operation})
+        (catch Exception e
+            (println e))))
+
+(defn record-job-results
+    [job-name datetime results]
+    (try
+        (jdbc/insert! db-spec/emender-service-db
+                      :results {:job job-name :datetime datetime :results results})
+        (catch Exception e
+            (println e))))
+
