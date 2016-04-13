3029967eac9f17f9bd58e8a10f32db72d194709c
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-04-13 13:48:12 +0200

    Additional test modules: rest_api_test.clj results_test.clj

---
+++ test/emender_service/rest_api_test.clj
@@ -0,0 +1,4 @@
+(ns emender-service.rest-api-test
+  (:require [clojure.test :refer :all]
+            [emender-service.rest-api :refer :all]))
+
---
+++ test/emender_service/results_test.clj
@@ -0,0 +1,4 @@
+(ns emender-service.results-test
+  (:require [clojure.test :refer :all]
+            [emender-service.results :refer :all]))
+
