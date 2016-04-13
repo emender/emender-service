e41245926cffb2ec22830be1b5429bdcd1cd13b6
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-04-13 17:20:36 +0200

    Script to list content of the whole DB schema

---
+++ database/list_all.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo "Log"
+./list_log.sh
+echo ""
+echo ""
+
+echo "Errors"
+./list_errors.sh
+echo ""
+echo ""
+
+echo "Requests"
+./list_requests.sh
+echo ""
+echo ""
+
+echo "Jobs"
+./list_jobs.sh
+echo ""
+echo ""
+
+echo "Results"
+./list_results.sh
+echo ""
+echo ""
+
