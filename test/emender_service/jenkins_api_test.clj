370859fc977289eaf150cd30810b4f4c6b1d465a
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-04-13 13:47:51 +0200

    Additional test modules: jenkins_api_test.clj middleware_test.clj

---
+++ test/emender_service/jenkins_api_test.clj
@@ -0,0 +1,4 @@
+(ns emender-service.jenkins-api-test
+  (:require [clojure.test :refer :all]
+            [emender-service.jenkins-api :refer :all]))
+
---
+++ test/emender_service/middleware_test.clj
@@ -0,0 +1,4 @@
+(ns emender-service.middleware-test
+  (:require [clojure.test :refer :all]
+            [emender-service.middleware :refer :all]))
+
