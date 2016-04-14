8ac507df0bf2acd8a130c3d36939368a89b8beee
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-04-14 11:42:18 +0200

    Negative test (wrong branch)

--- api_calls/run-test-bad-branch.sh
+++ api_calls/run-test-bad-branch.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
-curl -X POST --header "Content-Type: application/json" -d "@testinfo1.json" -v "localhost:3000/v1/run-test/doc-Other-Book+(test)"
+curl -X POST --header "Content-Type: application/json" -d "@testinfo-bad-branch.json" -v "localhost:3000/v1/run-test/doc-Broken-Book+(test)"
 
