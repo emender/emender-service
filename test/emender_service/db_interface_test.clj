31873cc5cf775646aaf416ff3a2ffac3084d229b
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-04-18 15:52:30 +0200

    Added callable? function into various test modules (to be used later)

--- test/emender_service/db_interface_test.clj
+++ test/emender_service/db_interface_test.clj
@@ -2,3 +2,12 @@
   (:require [clojure.test :refer :all]
             [emender-service.db-interface :refer :all]))
 
+;
+; Common functions used by tests.
+;
+
+(defn callable?
+    "Test if given function-name is bound to the real function."
+    [function-name]
+    (clojure.test/function? function-name))
+
